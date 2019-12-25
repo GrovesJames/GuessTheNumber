ec079b498aa36b4c4df4d7d11bb2fa41b3723540
James <triskept@gmail.com>
2019-12-25 11:42:19 -0500

    added user attempt counter as well as a way to check remaining tries. Possible further refactor incoming

--- guessingGame/Program.cs
+++ guessingGame/Program.cs
@@ -6,36 +6,58 @@ namespace guessingGame
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+            int randomNumber = random.Next(1, 11);
+            int attempts = 4;
             bool game = true;
 
             while (game)
             {
+                Console.WriteLine("--------------------");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("\nGuessing game!\n");
                 Console.WriteLine("Guess a number between 1-10");
-                Console.WriteLine("Enter 0 to get instructions");
+                Console.WriteLine("Enter 0 to get instructions\n\n");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("--------------------");
+
+                attempts = attempts - 1;
+                if (attempts == 0)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Thanks for trying! Game over.");
+                }
+                else
+                {
+                    Console.WriteLine("\nYou have " + attempts + " attempts remaining!\n\n");
+                }
                 string guess = Console.ReadLine();
                 int guessNum = Convert.ToInt32(guess);
+                Console.Clear();
 
-                if (guessNum == 7)
+                if (guessNum == randomNumber)
                 {
-                    Console.WriteLine("You right nigga");
+                    Console.Clear();
+                    Console.WriteLine("You're correct!");
+                    Console.ReadLine();
+                    game = false;
                 }
                 else if (guessNum == 0)
                 {
-                    Console.WriteLine("You guess 1-10 nigga whatchu mean??");
+                    Console.WriteLine("You guess 1-10, don't overthink it!\n\n");
                 }
                 else if (guessNum == -1)
                 {
                     game = false;
                 }
-                else if (guessNum <= 6)
+                else if (guessNum <= randomNumber - 1)
                 {
-                    Console.WriteLine("Guess higher");
+                    Console.WriteLine("Guess higher\n\n");
                 }
-                else if (guessNum >= 8)
+                else if (guessNum >= randomNumber + 1)
                 {
-                    Console.WriteLine("Guess lower");
+                    Console.WriteLine("Guess lower\n\n");
                 }
-
             }
         }
     }
